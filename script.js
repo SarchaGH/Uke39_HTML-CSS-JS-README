88770de43642f4c00977e551c4cdf58cb0615556
SarchaGH <dorado48178@gmail.com>
2026-09-25 15:02:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+document.write("<h1>Travel Thai</h1>");  //viser text//
+document.write("<h3>Choses the Region</h3>");
+alert("Welcome!");
+
+ //######################################################################################################//
+
+let Region = //Vr//
+ Option = (Northen)
+ Option = (Southern)
+ Option = (Central)
+ Option = (Northeastern)
+ Option = (Eastern)
+ Option = (Western)
+
+ 
+ //document.write("You chose: " ,T_style);
+//console.log(typeof(T_style));
+
+
+
+/*
+const carSelect = document.getElementById('car-select');
+const carResult = document.getElementById('car-result');
+/*
+carSelect.addEventListener('change', () => {
+  const chosenCar = carSelect.value;
+  
+  if (chosenCar !== 'none') {
+    carResult.textContent = `You picked: ${chosenCar}`;
+  } else {
+    carResult.textContent = '';
+  }
+});
+
+/*
+function getresult(){}
+  const selectElement =documentByid("A1")
+  const selectvalue =selectElementElement.value;
+    let message="";
+  
+     if selectvalue ===A1
+      var document.write("You chose:"(selectvalue))
+ //document.write("You chose: " ,T_style);
+//console.log(typeof(T_style));
+
+
+/*
+  //#######This is just exp.######## I will put it in README file//
+alert("Welcome!");
+console.log(" Exp console");// text i console//
+console.error("Exp error");//error//
+console.warn("Exp warnning");//warnning//
+
+
+
+<label for="car-select">Choose a car manufacturer:</label>
+<select id="car-select">
+  <option value="none">-- Please select an option --</option>
+  <option value="Tesla">Tesla</option>
+  <option value="Volvo">Volvo</option>
+  <option value="Toyota">Toyota</option>
+</select>
+   <script src="script.js"></script>
+*/
